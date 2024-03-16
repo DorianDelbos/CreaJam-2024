1a6ae31aa5129382c2a6290ca444808a67be46fc
naoki18 <jadras.dunkan@gmail.com>
2024-03-16 09:06:13 +0100

    DORIAN TIENS

--- DeeperAndDeeper/Assets/AssetsDeeperAndDeeper/Code/Entities/Car/CrankLight.cs
+++ DeeperAndDeeper/Assets/AssetsDeeperAndDeeper/Code/Entities/Car/CrankLight.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GloveBox : MonoBehaviour, ISelection
+public class CrankLight : MonoBehaviour, ISelection
 {
     private bool isHover = false;
     private bool isSelected = false;
@@ -14,22 +14,22 @@ public class GloveBox : MonoBehaviour, ISelection
     public Vector3 BasePos { get => basePos; set => basePos = value; }
     public Quaternion BaseRot { get => baseRot; set => baseRot = value; }
     public float OffsetCamera { get => offsetCamera; set => offsetCamera = value; }
+
     public void OnClick()
     {
         InteriorVehicle vehicle = GameObject.FindFirstObjectByType<InteriorVehicle>();
-        vehicle.ToggleGloveBox();
-
+        vehicle.ToggleLight();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        (this as ISelection).Update();
+        
     }
 }
