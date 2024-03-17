ee1b27b327886c04dfe21b8e34f0aa5bb4f93956
Dorian <thesothief@gmail.com>
2024-03-17 02:09:03 +0100

    t

---
+++ DeeperAndDeeper/Assets/AssetsDeeperAndDeeper/Code/Objects/Wardrobe.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Wardrobe : MonoBehaviour, ISelection
+{
+    #region ISelection
+    private bool isHover = false;
+    private bool isSelected = false;
+    private Vector3 basePos;
+    private Quaternion baseRot;
+    float offsetCamera = 0;
+    public bool IsHover { get => isHover; set => isHover = value; }
+    public bool IsSelected { get => isSelected; set => isSelected = value; }
+    public Vector3 BasePos { get => basePos; set => basePos = value; }
+    public Quaternion BaseRot { get => baseRot; set => baseRot = value; }
+    public float OffsetCamera { get => offsetCamera; set => offsetCamera = value; }
+    #endregion
+
+    [SerializeField] private Door[] doors;
+
+    public void OnClick()
+    {
+        foreach (Door door in doors)
+        {
+            door.ToggleDoor();
+        }
+    }
+
+    void Update()
+    {
+        (this as ISelection).Update();
+    }
+}
