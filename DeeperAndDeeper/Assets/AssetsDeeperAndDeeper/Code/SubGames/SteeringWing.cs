6a93bff3b6f15f9f62e39d94e7122e24e7e1e0c7
naoki18 <jadras.dunkan@gmail.com>
2024-03-17 06:39:34 +0100

    sub1

--- DeeperAndDeeper/Assets/AssetsDeeperAndDeeper/Code/SubGames/SteeringWing.cs
+++ DeeperAndDeeper/Assets/AssetsDeeperAndDeeper/Code/SubGames/SteeringWing.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class SteeringWing : MonoBehaviour, ISelection
 {
+    enum Difficulty
+    {
+        HARD,
+        MEDIUM,
+        EASY
+        
+    }
     Vector3 rotationZ = Vector3.zero;
 
     private bool isHover = false;
     private bool isSelected = false;
     private Vector3 basePos;
     private Quaternion baseRot;
-
     [SerializeField] RectTransform arrowImage;
+    [SerializeField] RectTransform indicatorImage;
+    [SerializeField] List<Image> images;
+    Difficulty difficulty = Difficulty.HARD;
+    int[] angleDifficulty = new int[3] { 15, 30, 45 };
     float offsetCamera = 0;
     public bool IsHover { get => isHover; set => isHover = value; }
     public bool IsSelected { get => isSelected; set => isSelected = value; }
@@ -20,6 +33,7 @@ public class SteeringWing : MonoBehaviour, ISelection
     public Quaternion BaseRot { get => baseRot; set => baseRot = value; }
     public float OffsetCamera { get => offsetCamera; set => offsetCamera = value; }
 
+    bool isRotating = false;
     public void OnClick()
     {
         InteriorVehicle vehicle = GameObject.FindFirstObjectByType<InteriorVehicle>();
@@ -38,11 +52,11 @@ public class SteeringWing : MonoBehaviour, ISelection
         if(GameManager.instance.GameState == GameManager.State.SUB_GAME_1)
         {
             #region Wing & Arrow Rotation
-            if (Input.GetKey(KeyCode.D) && this.transform.rotation.z < 90f)
+            if (Input.GetKey(KeyCode.D))
             {
                 rotationZ += new Vector3(0, 0, 1);
             }
-            else if (Input.GetKey(KeyCode.A) && this.transform.rotation.z > -90f)
+            else if (Input.GetKey(KeyCode.A))
             {
                 rotationZ -= new Vector3(0, 0, 1);
                 
@@ -51,10 +65,61 @@ public class SteeringWing : MonoBehaviour, ISelection
             this.transform.localEulerAngles = rotationZ;
             arrowImage.transform.eulerAngles = -rotationZ;
             #endregion
+
+            // Nique le POO sorry je fais n'importe quoi <3
+
+            #region SUB_GAME1
+
+            if (!isRotating) StartCoroutine(RotateIndicator());
+            if(Quaternion.Angle(arrowImage.rotation, indicatorImage.rotation) > angleDifficulty[(int)difficulty]) 
+            {
+            }
+            else
+            {
+            }
+            #endregion
         }
         else
         {
             (this as ISelection).Update();
         }
+    }
+
+    IEnumerator RotateIndicator()
+    {
+        isRotating = true;
+        float duringTime = Random.Range(0.5f, 1.5f);
+        float currentTime = 0;
+        bool isRight = Random.value < 0.5;
+        Vector3 currentRotIndicator = indicatorImage.transform.localEulerAngles;
+        if (currentRotIndicator.z > 180)
+        {
+            currentRotIndicator.z = 180 - currentRotIndicator.z;
+        }
+        while (currentTime < duringTime)
+        {
+            currentTime += Time.deltaTime;
+            if (isRight)
+                currentRotIndicator -= new Vector3(0,0,0.5f);
+            else
+                currentRotIndicator += new Vector3(0, 0, 0.5f);
+            Debug.Log(currentRotIndicator.z);
+            currentRotIndicator.z = Mathf.Clamp(currentRotIndicator.z, -70, 70);
+            indicatorImage.transform.localEulerAngles = currentRotIndicator;
+            yield return null;
+        }
+        isRotating = false;
+    }
+
+    void Lose()
+    {
+        arrowImage.rotation = Quaternion.Euler(Vector3.zero);
+        indicatorImage.rotation = Quaternion.Euler(Vector3.zero);
+        if(difficulty != Difficulty.EASY)
+        {
+            difficulty--;
+        }
+        // indicatorImage.GetComponent<Image>(). = images[(int)difficulty]
+        // TODO : change source image difficulty
     }
 }
